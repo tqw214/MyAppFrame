e8ae500464fdeddafc30b95eae670f0adf5cd82f
tqw214 <tqw214@163.com>
2020-03-15 11:04:20 +0800

    commit

--- dagger/src/main/java/com/viger/dagger/MainActivity.java
+++ dagger/src/main/java/com/viger/dagger/MainActivity.java
@@ -2,12 +2,9 @@ package com.viger.dagger;
 
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-import com.viger.dagger.bean.Student;
-import com.viger.dagger.component.DaggerMainComponent;
-import com.viger.dagger.module.MainModule;
 
-import javax.inject.Inject;
+import androidx.appcompat.app.AppCompatActivity;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -16,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.textView)
     TextView textView;
 
-    @Inject
-    Student student;
+  //  @Inject
+   // Student student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        DaggerMainComponent.builder().mainModule(new MainModule()).build().inject(this);
+        //DaggerMainComponent.builder().mainModule(new MainModule()).build().inject(this);
 
-        textView.setText(student.showMessage());
+      //  textView.setText(student.showMessage());
     }
 }
